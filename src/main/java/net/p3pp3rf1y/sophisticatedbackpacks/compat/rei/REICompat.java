3ed1dff8f48328d7b83155ce756cf355df062614
Salandora <salandora@gmail.com>
2023-08-02 17:23:12 +0200

    Implemented GridMenuInfo for REI for the crafting upgrade

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/rei/REICompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/rei/REICompat.java
@@ -3,11 +3,16 @@ package net.p3pp3rf1y.sophisticatedbackpacks.compat.rei;
 import me.shedaniel.rei.api.common.entry.comparison.EntryComparator;
 import me.shedaniel.rei.api.common.entry.comparison.ItemComparatorRegistry;
 import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
+import me.shedaniel.rei.api.common.transfer.info.MenuInfoRegistry;
+import me.shedaniel.rei.api.common.transfer.info.simple.SimpleMenuInfoProvider;
+import me.shedaniel.rei.plugin.common.BuiltinPlugin;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.world.item.ItemStack;
+import net.p3pp3rf1y.sophisticatedbackpacks.common.gui.BackpackContainer;
 import net.p3pp3rf1y.sophisticatedbackpacks.common.lookup.BackpackWrapperLookup;
 import net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems;
+import net.p3pp3rf1y.sophisticatedcore.compat.rei.ReiGridMenuInfo;
 
 import java.util.function.Function;
 
@@ -37,5 +42,10 @@ public class REICompat implements REIServerPlugin {
                 ModItems.DIAMOND_BACKPACK.get(),
                 ModItems.NETHERITE_BACKPACK.get()
         );
+    }
+
+    @Override
+    public void registerMenuInfo(MenuInfoRegistry registry) {
+        registry.register(BuiltinPlugin.CRAFTING, BackpackContainer.class, SimpleMenuInfoProvider.of(ReiGridMenuInfo::new));
     }
 }
